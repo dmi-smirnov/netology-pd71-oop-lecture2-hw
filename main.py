7935a0bfa52601a0b0fab62739ff950860390c7c
Dmitriy Smirnov <git@d-sm.ru>
2022-11-29 21:16:09 +0300

    task2-completed

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+    def rate_lecturer(self, lecturer, course, grade):
+        if (course in self.courses_in_progress
+        and isinstance(lecturer, Lecturer)
+        and course in lecturer.courses_attached):
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
 
 
 class Mentor:
@@ -14,8 +24,15 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+        if isinstance(student, Student) and course in student.courses_in_progress:
             if course in student.grades:
                 student.grades[course] += [grade]
             else:
@@ -23,15 +40,20 @@ class Mentor:
         else:
             return 'Ошибка'
 
-
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
+#
+cool_lecturer = Lecturer('Some', 'Buddy')
+cool_lecturer.courses_attached += ['Python']
 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+cool_reviewer = Reviewer('Some', 'Dubby')
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+
+best_student.rate_lecturer(cool_lecturer, 'Python', 9)
+best_student.rate_lecturer(cool_lecturer, 'Python', 9)
 
 print(best_student.grades)
+print(cool_lecturer.grades)
